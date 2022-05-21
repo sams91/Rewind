8a5dd0447e3886a4bdc79a76f4a566c43c6078f2
sams91 <103255663+sams91@users.noreply.github.com>
2022-05-21 16:06:30 -0500

    Added an about page and finished the app.

--- js/site.js
+++ js/site.js
@@ -1,5 +1,44 @@
+//get the string from the page
+//controller function
+function getValue() {
 
-// call hello world
-function helloWorld(){
-    alert("hello World");
+    document.getElementById("alert").classList.add("invisible")
+   let userString = document.getElementById("userString").value;
+
+   let revString = reverseString(userString);
+
+   displayString(revString);
+}
+
+//reverse the string
+//logic function
+function reverseString(userString) {
+
+    let revString = [];
+
+
+    // reverse a string using a for loop
+    //AKA "decrementing for loop". 
+    //This starts at the end of the array and goes backwards to 
+    //zero rather than starting at zero and going forward.
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+        
+    }
+
+    return revString;
+
+}
+
+//display the reversed string to the user
+//view function
+function displayString(revString) {
+
+
+
+    //write to the page
+    document.getElementById("message").innerHTML = `Your String Reversed Is: ${revString}`;
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+
 }
